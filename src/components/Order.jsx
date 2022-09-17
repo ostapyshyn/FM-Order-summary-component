df65e3455c36312990febd5dce2788174d5420b1
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-09-17 17:52:14 +0300

    Finish card

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import styles from '../assets/scss/order.module.scss';
 import { ReactComponent as Hero } from '../assets/svg/illustration-hero.svg';
+import { ReactComponent as Music } from '../assets/svg/icon-music.svg';
 
 function Order() {
   return (
     <main>
       <section>
         <Hero />
-        <h1>Order Summary</h1>
-        <p>
-          You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere
-          you like!
-        </p>
-        <p>Annual Plan</p>
-        <p>$59.99/year</p>
-        <a href="#">Change</a>
-        <button>Proceed to Payment</button>
-        <a href="#">Cancel Order</a>
+        <article>
+          <h1>Order Summary</h1>
+          <p className={styles.info}>
+            You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere
+            you like!
+          </p>
+          <div className={styles.plan}>
+            <div className={styles.plan_price}>
+              <Music />
+              <div className={styles.annual_price}>
+                <p className={styles.annual}>Annual Plan</p>
+                <p className={styles.price}>$59.99/year</p>
+              </div>
+            </div>
+
+            <a
+              className={styles.change}
+              href="https://github.com/ostapyshyn/FM-Order-summary-component">
+              Change
+            </a>
+          </div>
+
+          <button>Proceed to Payment</button>
+          <a
+            className={styles.cancel}
+            href="https://github.com/ostapyshyn/FM-Order-summary-component">
+            Cancel Order
+          </a>
+        </article>
       </section>
     </main>
   );
